84133628a92084a1a5949e41af631aa3016bf7cd
Kenta Ogawa <gakenshizu1199@gmail.com>
2023-11-02 08:16:04 +0900

    add calc trace

---
+++ calc_trace.py
@@ -0,0 +1,15 @@
+import jax
+import jax.numpy as jnp
+
+
+def calc_trace(
+    Kinvy: jnp.array, dKdtheta: jnp.array, probe_vectors: jnp.array, n_tridiag: int
+):
+    return (
+        jnp.einsum(
+            "ij, ij ->",
+            Kinvy[:, :n_tridiag],
+            jnp.einsum("ij, jk->ik", dKdtheta, probe_vectors),
+        )
+        / n_tridiag
+    )
