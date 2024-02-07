ae3ad9f7c8cfc4845cc56d19178857c503ecca75
Kenta Ogawa <gakenshizu1199@gmail.com>
2024-02-07 12:50:26 +0900

    [update]minor

--- tests/utils/test_calc_loss_pigp_linearop.py
+++ tests/utils/test_calc_loss_pigp_linearop.py
@@ -124,32 +124,32 @@ from stopro.GP.gp_stokes_3D_naive import GPStokes3DNaive
 #     )
 
 
-# def test_loss_sp_drag3D_only_v_11808_rowwise():
-#     project_name = "tests/data"
-#     simulation_name = "20240207_drag3D_only_v_11808"
-#     init = None
-#     scale = 10.0
-#     kwargs_setup_loss = {
-#         "rank": 0,
-#         "n_tridiag": 20,
-#         "max_tridiag_iter": 40,
-#         "cg_tolerance": 0.01,
-#         "max_iter_cg": 2000,
-#         "min_preconditioning_size": 1,
-#     }
-#     calc_loss_sin(
-#         project_name,
-#         simulation_name,
-#         init,
-#         scale,
-#         test_gpytorch=False,
-#         kwargs_setup_loss=kwargs_setup_loss,
-#         gp_class=GPStokes3DNaive,
-#         use_lazy_matrix=True,
-#         matmul_blockwise=False,
-#         test_cholesky=False,
-#         test_ours=True,
-#     )
+def test_loss_sp_drag3D_only_v_11808_rowwise():
+    project_name = "tests/data"
+    simulation_name = "20240207_drag3D_only_v_11808"
+    init = None
+    scale = 10.0
+    kwargs_setup_loss = {
+        "rank": 0,
+        "n_tridiag": 20,
+        "max_tridiag_iter": 40,
+        "cg_tolerance": 0.01,
+        "max_iter_cg": 2000,
+        "min_preconditioning_size": 1,
+    }
+    calc_loss_sin(
+        project_name,
+        simulation_name,
+        init,
+        scale,
+        test_gpytorch=False,
+        kwargs_setup_loss=kwargs_setup_loss,
+        gp_class=GPStokes3DNaive,
+        use_lazy_matrix=True,
+        matmul_blockwise=False,
+        test_cholesky=True,
+        test_ours=False,
+    )
 
 
 # def test_loss_sp_drag3D_only_v_11808_blockwise():
@@ -206,32 +206,32 @@ from stopro.GP.gp_stokes_3D_naive import GPStokes3DNaive
 #     )
 
 
-def test_loss_sp_drag3D_only_v_23088_rowwise():
-    project_name = "tests/data"
-    simulation_name = "20240207_drag3D_only_v_23088"
-    init = None
-    scale = 20.0
-    kwargs_setup_loss = {
-        "rank": 0,
-        "n_tridiag": 20,
-        "max_tridiag_iter": 40,
-        "cg_tolerance": 0.01,
-        "max_iter_cg": 2000,
-        "min_preconditioning_size": 1,
-    }
-    calc_loss_sin(
-        project_name,
-        simulation_name,
-        init,
-        scale,
-        test_gpytorch=False,
-        kwargs_setup_loss=kwargs_setup_loss,
-        gp_class=GPStokes3DNaive,
-        use_lazy_matrix=True,
-        matmul_blockwise=False,
-        test_cholesky=False,
-        test_ours=True,
-    )
+# def test_loss_sp_drag3D_only_v_23088_rowwise():
+#     project_name = "tests/data"
+#     simulation_name = "20240207_drag3D_only_v_23088"
+#     init = None
+#     scale = 20.0
+#     kwargs_setup_loss = {
+#         "rank": 0,
+#         "n_tridiag": 20,
+#         "max_tridiag_iter": 40,
+#         "cg_tolerance": 0.01,
+#         "max_iter_cg": 2000,
+#         "min_preconditioning_size": 1,
+#     }
+#     calc_loss_sin(
+#         project_name,
+#         simulation_name,
+#         init,
+#         scale,
+#         test_gpytorch=False,
+#         kwargs_setup_loss=kwargs_setup_loss,
+#         gp_class=GPStokes3DNaive,
+#         use_lazy_matrix=True,
+#         matmul_blockwise=False,
+#         test_cholesky=False,
+#         test_ours=True,
+#     )
 
 
 # def test_loss_sp_drag3D_8794():
